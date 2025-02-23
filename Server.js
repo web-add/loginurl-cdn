98e1965a701715b9e9a9d1e8303c3873d68d233e
GTPSHAX <rafiehasannudin@gmail.com>
2025-02-22 22:15:35 -0800

    Adding multi SSL using SNICallback

--- Server.js
+++ Server.js
@@ -3,16 +3,40 @@ const path = require('path');
 const app = require(path.join(__dirname, 'MainApp.js'));
 const https = require('https');
 const fs = require('fs');
+const tls = require('tls');
 
-// starting server with port 3000
-app.listen(3000, () => {
+const sniDefaultCert = fs.readFileSync(path.join(__dirname, 'certs/_wildcard.gtps.pw.pem'));
+const sniDefaultKey = fs.readFileSync(path.join(__dirname, 'certs/_wildcard.gtps.pw-key.pem'));
+
+const sniCallback = (serverName, callback) => {
+	console.log(serverName);
+	let cert = null;
+	let key = null;
+
+	if (serverName != 'www.growtopia1.com' && serverName != 'www.growtopia2.com') {
+        cert = sniDefaultCert;
+		key = sniDefaultKey;
+	} else {
+        cert = fs.readFileSync(path.join(__dirname, 'certs/gt.pem'));
+		key = fs.readFileSync(path.join(__dirname, 'certs/gt-key.pem'));
+	}
+
+	callback(null, new tls.createSecureContext({
+		cert,
+		key,
+	}));
+}
+
+const serverOptions = {
+	SNICallback: sniCallback,
+}
+
+// starting server with port 80
+app.listen(80, () => {
     console.log('Server started at http://localhost:3000');
 });
 
 // starting secure server with port 443
-https.createServer({
-    key: fs.readFileSync(path.join(__dirname, 'cert', 'localhost-key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert', 'localhost.pem'))
-}, app).listen(443, () => {
+https.createServer(serverOptions, app).listen(443, () => {
     console.log('Secure server started at https://localhost:443');
 });
